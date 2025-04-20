2728f170a5415183f156dd9d010226b173fbbd5f
rahulydv <ry040799@gmail.com>
2025-04-20 17:45:47 +0530

    basic backend

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/login', (req, res)=>{
+    console.log(req.body);
+    let message = 'Invalid credentials';
+    if(req.body.email==='abc@gmail.com' && req.body.password==='abc'){
+        // res.send('http://localhost:5173/FrontPage');
+        message = 'FrontPage';
+    }
+    res.send({message});
+})
 
 app.listen(8000, ()=>{
     console.log(`Listening on port 8000`);
