2b0bfaf315022cb26648658687f6634860b4ba03
rahulydv <ry040799@gmail.com>
2025-05-07 19:34:34 +0530

    Auth work

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
 app.post('/login', (req, res)=>{
-    console.log(req.body);
-    let message = 'Invalid credentials';
-    if(req.body.email==='abc@gmail.com' && req.body.password==='abc'){
-        message = 'FrontPage';
+    const {email, password} = req.body;
+
+    if(!req.body.email==='abc@gmail.com' || !req.body.password==='abc'){
+        res.sendStatus(401);
+        return;
     }
-    res.send({message});
+    let accessToken = jwt.sign({email, password}, process.env.ACCESS_TOKEN_SECRET);
+    res.cookie('jwt', accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 24*60*60*1000 // 1 day
+    });
+    res.sendStatus(200);
 })
+
+app.post('/verifyAuth', authenticateToken, (req, res)=>{
+    res.sendStatus(200);
+})
+
+app.post('/logout', (req, res)=>{
+    res.clearCookie('jwt', {
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: true
+    });
+    res.sendStatus(200);
+});
+
+
+function authenticateToken(req, res, next){
+    const token = req.cookies.jwt;
+    if(!token) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err) return res.sendStatus(403);
+        req.user = decoded;
+        decoded.password = 'abc';
+        next();
+    })
+}
 
 app.listen(8000, ()=>{
     console.log(`Listening on port 8000`);
